a9ae446d3357d4505ca8f0816e5a3d3aab5ace3f
Ross Kinsman <ross.kinsman@inviqa.com>
2019-06-10 13:58:18 +0100

    Use alternate Xdebug installation method on PHP 5.6
    Uses workaround detailed in github.com/docker-library/php/issues/133 to
    compile Xdebug from source with different args. Allows debugging of code
    involving static properties without errors.

--- installer/stretch/extensions/xdebug.sh
+++ installer/stretch/extensions/xdebug.sh
@@ -4,7 +4,22 @@ function install_xdebug()
 {
     case "$VERSION" in
             "5.6")
-                printf "\n" | pecl install xdebug-2.5.5
+                BEFORE_PWD=$(pwd) \
+                    && mkdir -p /opt/xdebug \
+                    && cd /opt/xdebug \
+                    && curl https://xdebug.org/files/xdebug-2.5.5.tgz -o xdebug-2.5.5.tgz \
+                    && echo "72108bf2bc514ee7198e10466a0fedcac3df9bbc5bd26ce2ec2dafab990bf1a4" "xdebug-2.5.5.tgz" | sha256sum --check \
+                    && tar -xzvf xdebug-2.5.5.tgz \
+                    && cd xdebug-2.5.5 \
+                    && phpize \
+                    && ./configure --enable-xdebug \
+                    && make clean \
+                    && sed -i 's/-O2/-O0/g' Makefile \
+                    && make \
+                    && make test \
+                    && make install \
+                    && cd "${BEFORE_PWD}" \
+                    && rm -r /opt/xdebug
                 ;;
             *)
                 printf "\n" | pecl install xdebug
