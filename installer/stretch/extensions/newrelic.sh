51efb05bdf2ff37a190c0ef018e7bfedc9a94d9a
dcole-inviqa <dcole@inviqa.com>
2019-10-08 12:44:14 +0100

    MISC fix NewRelic installer (#22)

--- installer/stretch/extensions/newrelic.sh
+++ installer/stretch/extensions/newrelic.sh
@@ -2,7 +2,7 @@
 
 function install_newrelic()
 {
-    curl -L http://download.newrelic.com/php_agent/release/newrelic-php5-9.1.0.246-linux.tar.gz | tar -C /tmp -zx
+    curl -L https://download.newrelic.com/php_agent/archive/9.1.0.246/newrelic-php5-9.1.0.246-linux.tar.gz | tar -C /tmp -zx
 
     export NR_INSTALL_USE_CP_NOT_LN=1
     export NR_INSTALL_SILENT=1
