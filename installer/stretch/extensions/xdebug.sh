040fa6ac43e34fc7308231211ae0ab6b70469465
Andy Thompson <athompson@inviqa.com>
2020-02-07 09:21:53 +0000

    Revert xdebug PHP 5.6 changes and add a comment as to why

--- installer/stretch/extensions/xdebug.sh
+++ installer/stretch/extensions/xdebug.sh
@@ -6,14 +6,38 @@ function install_xdebug()
     case "$VERSION" in
             "5.6")
                 XDEBUG_PACKAGE="xdebug-2.5.5"
+                # Build xdebug manually to avoid a debian compiler bug
+                # https://github.com/docker-library/php/issues/133
+                pushd /usr/src
+
+                curl "https://xdebug.org/files/${XDEBUG_PACKAGE}.tgz" -o "${XDEBUG_PACKAGE}.tgz"
+                echo "72108bf2bc514ee7198e10466a0fedcac3df9bbc5bd26ce2ec2dafab990bf1a4" "${XDEBUG_PACKAGE}.tgz" | sha256sum --check
+                tar -xzvf "${XDEBUG_PACKAGE}.tgz"
+
+                pushd "${XDEBUG_PACKAGE}"
+                phpize
+                ./configure --enable-xdebug
+                make clean
+                # compiler option -O2 changed to -O0
+                sed -i 's/-O2/-O0/g' Makefile
+                make
+                make test
+                make install
+                popd
+
+                rm -r "${XDEBUG_PACKAGE}" "${XDEBUG_PACKAGE}.tgz"
+
+                popd
+
                 ;;
             "7.0")
                 XDEBUG_PACKAGE="xdebug-2.8.1"
-                ;;
+                ;&
             *)
-    esac
+                printf "\n" | pecl install "$XDEBUG_PACKAGE"
 
-    printf "\n" | pecl install "$XDEBUG_PACKAGE"
+                docker-php-ext-enable xdebug
+    esac
 
     docker-php-ext-enable xdebug
 }
