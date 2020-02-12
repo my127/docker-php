ae75de52cc4640194ab9d0262aa236999195351f
Kieren Evans <kevans@inviqa.com>
2020-02-12 18:17:56 +0200

    Add header text per extension

--- test.sh
+++ test.sh
@@ -3,8 +3,10 @@
 set -e -o pipefail
 
 for service in $(docker-compose config --services | grep base); do
+  echo
+  echo "Testing service ${service}"
   # shellcheck disable=SC2016
-  docker-compose run --rm "$service" bash -e -c 'cd /root/installer/; for extension in extensions/*; do ./enable.sh "$(expr match "$extension" "extensions/\(.*\).sh")" > /tmp/ext-install.log 2>&1 || (cat /tmp/ext-install.log && exit 1); done; php -m; php -v'
+  docker-compose run --rm "$service" bash -e -c 'cd /root/installer/; for extension in extensions/*; do echo -n "Installing ${extension}..."; ./enable.sh "$(expr match "$extension" "extensions/\(.*\).sh")" > /tmp/ext-install.log 2>&1 || (echo " failure" && cat /tmp/ext-install.log && exit 1); echo " success"; done; php -m; php -v'
 done
 
 for service in $(docker-compose config --services | grep console); do
