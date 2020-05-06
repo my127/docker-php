7e427babd8920d97727bfb28dfe468cf0e29066b
Kieren Evans <kevans@inviqa.com>
2020-05-06 23:38:52 +0300

    Add Buster-based 7.3 image for akeneo's requirement of ghostscript 9.27

--- installer/buster/extensions/gd.sh
+++ installer/buster/extensions/gd.sh
@@ -5,10 +5,20 @@ function install_gd()
     _gd_deps_runtime
     _gd_deps_build
 
-    docker-php-ext-configure gd \
-      --enable-gd \
-      --with-freetype \
-      --with-jpeg
+    case "$VERSION" in
+            "7.3")
+                docker-php-ext-configure gd \
+                  --with-gd \
+                  --with-freetype-dir=/usr/include/ \
+                  --with-png-dir=/usr/include/ \
+                  --with-jpeg-dir=/usr/include/
+                ;;
+            *)
+                docker-php-ext-configure gd \
+                  --enable-gd \
+                  --with-freetype \
+                  --with-jpeg
+    esac
 
     docker-php-ext-install gd
 
