6fc4c11d7fea7e2e0ef2f7c3269becb26b95b414
Kieren Evans <kierenevans@users.noreply.github.com>
2020-09-17 17:22:49 +0300

    Fix protobuf package version for 5.6 (#35)

--- installer/stretch/extensions/protobuf.sh
+++ installer/stretch/extensions/protobuf.sh
@@ -2,6 +2,13 @@
 
 function install_protobuf()
 {
-    pecl install protobuf
+    case "$VERSION" in
+            "5.6")
+                printf "\n" | pecl install protobuf-3.12.4
+                ;;
+            *)
+                printf "\n" | pecl install protobuf
+    esac
+
     docker-php-ext-enable protobuf
 }
