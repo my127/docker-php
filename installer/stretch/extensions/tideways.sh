a2d7f5adaf6a723cca57851deb0ab1d18db2f31c
Kieren Evans <kevans@inviqa.com>
2020-09-20 15:43:35 +0300

    Add tideways extension

---
+++ installer/stretch/extensions/tideways.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function install_tideways()
+{
+    _tideways_deps_build
+    _tideways_deps_runtime
+}
+
+function _tideways_deps_runtime()
+{
+    local CLI_PACKAGE=''
+    if [[ "$IMAGE_TYPE" == "console" ]]; then
+        CLI_PACKAGE='tideways-cli'
+    fi
+
+    install \
+      tideways-php \
+      "$CLI_PACKAGE"
+}
+
+function _tideways_deps_build()
+{
+    install \
+      apt-transport-https \
+      gnupg2
+
+    echo 'deb https://packages.tideways.com/apt-packages debian main' > /etc/apt/sources.list.d/tideways.list
+    curl -L -sS 'https://packages.tideways.com/key.gpg' | apt-key add -
+}
