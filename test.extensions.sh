ddc96cdf9a59fcbd21c0902d9b69b5fabf9dedee
Kieren Evans <kevans@inviqa.com>
2020-09-23 02:13:00 +0300

    Don't fail PHP 5.6-7.1 for not having sodium

--- test.extensions.sh
+++ test.extensions.sh
@@ -4,6 +4,8 @@ set -e
 
 cd /root/installer/
 
+VERSION="$(echo "$PHP_VERSION" | cut -c 1,3)"
+
 before="$(php -m)$(php -v)"
 echo "Before: $before"
 
@@ -16,7 +18,13 @@ for extension in extensions/*; do
         cat /tmp/ext-install.log
         exit 1
     fi
-    if [ "$extension_name" = 'blackfire' ] || [ "$extension" = 'tideways' ]; then
+    # These extensions aren't enabled by default
+    if [ "$extension_name" = 'blackfire' ] || [ "$extension_name" = "newrelic" ] [ "$extension_name" = 'tideways' ]; then
+        echo ' success'
+        continue
+    fi
+    # Sodium only available for PHP 7.2+
+    if  [ "$extension_name" = 'sodium' ] && [ "$VERSION" -lt 72 ]; then
         echo ' success'
         continue
     fi
