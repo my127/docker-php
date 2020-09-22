6369f4728fa56a31997bfd11faf4aeed85e037ad
Kieren Evans <kevans@inviqa.com>
2020-09-22 23:15:46 +0300

    Apart from tideways and blackfire

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ for service in $(docker-compose config --services | grep -E "${BUILD}" | grep ba
   echo
   echo "Testing service ${service}"
   # shellcheck disable=SC2016
-  docker-compose run --rm "$service" bash -e -c 'cd /root/installer/; for extension in extensions/*; do echo -n "Installing ${extension}..."; ./enable.sh "$(expr match "$extension" "extensions/\(.*\).sh")" > /tmp/ext-install.log 2>&1 || (echo " failure" && cat /tmp/ext-install.log && exit 1); php -m | grep -i "^$extension$"; echo " success"; done; php -m; php -v'
+  docker-compose run --rm "$service" bash -e -c 'cd /root/installer/; for extension in extensions/*; do echo -n "Installing ${extension}..."; ./enable.sh "$(expr match "$extension" "extensions/\(.*\).sh")" > /tmp/ext-install.log 2>&1 || (echo " failure" && cat /tmp/ext-install.log && exit 1); if [ "$extension" != "extensions/tideways.sh" ] && [ "$extension" != "extensions/blackfire.sh" ]; then php -m | grep -i "^$extension\$"; fi; echo " success"; done; php -m; php -v'
 done
 
 for service in $(docker-compose config --services | grep -E "${BUILD}" | grep console); do
