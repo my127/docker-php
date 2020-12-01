0809125428227d8a0f436f766fab9bcfec5c6eff
Kieren Evans <kevans@inviqa.com>
2020-12-02 00:00:19 +0200

    Try to keep the build alive on travis

--- installer/stretch/precompile.sh
+++ installer/stretch/precompile.sh
@@ -10,16 +10,21 @@ source ./lib/functions.sh
 
 function main()
 {
+    local KEEPALIVE_PID
     for extension in extensions/*
     do
         extension_name="${extension%.sh}"
         extension_name="${extension_name#extensions/}"
         echo -n "Installing ${extension}..."
+        bash -c 'sleep 60 && echo -n "." && sleep 60 && echo -n "."' &
+        KEEPALIVE_PID="$!"
         if ! compile "$extension_name" > /tmp/ext-install.log 2>&1; then
             echo " failure"
             cat /tmp/ext-install.log
+            kill "$KEEPALIVE_PID"
             exit 1
         fi
+        kill "$KEEPALIVE_PID"
         echo " success"
     done
 
