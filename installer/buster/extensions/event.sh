405faad9761d3d700e79c67e006cc62cb26f5d05
Kieren Evans <kevans@inviqa.com>
2021-01-05 15:38:43 +0200

    Use latest event (3.0.2)

--- installer/buster/extensions/event.sh
+++ installer/buster/extensions/event.sh
@@ -15,15 +15,16 @@ function compile_event()
 {
     _event_deps_build
 
+    local PACKAGE_NAME="event"
     case "$VERSION" in
-            "8.0")
-                echo "Skipping event install, unsupported php version"
-                ;;
-            *)
-                if ! printf "\n" | pecl install event; then
-                    return 1
-                fi
+        "8.0")
+            PACKAGE_NAME="event-3.0.2"
+            ;;
     esac
+
+    if ! printf "\n" | pecl install "$PACKAGE_NAME"; then
+        return 1
+    fi
 
     _event_clean
 }
