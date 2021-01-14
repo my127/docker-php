09a2d809f2e1f866fe2b6a0e623cd1b5d7775ad7
Kieren Evans <kevans@inviqa.com>
2021-01-14 14:11:09 +0200

    Update newrelic extension to 9.15

--- installer/stretch/extensions/newrelic.sh
+++ installer/stretch/extensions/newrelic.sh
@@ -2,7 +2,7 @@
 
 function install_newrelic()
 {
-    curl -L https://download.newrelic.com/php_agent/archive/9.1.0.246/newrelic-php5-9.1.0.246-linux.tar.gz | tar -C /tmp -zx
+    curl -L https://download.newrelic.com/php_agent/archive/9.15.0.293/newrelic-php5-9.15.0.293-linux.tar.gz | tar -C /tmp -zx
 
     export NR_INSTALL_USE_CP_NOT_LN=1
     export NR_INSTALL_SILENT=1
