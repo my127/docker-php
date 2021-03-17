6cb1b13206278fe3f585bb08118635c947f1c874
Kieren Evans <kevans@inviqa.com>
2021-03-17 17:41:07 +0200

    Only pull if missing

--- quality.sh
+++ quality.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e -o pipefail
 
-docker pull koalaman/shellcheck-alpine:stable
+if ! docker image ls -a --format '{{ print .Repository ":" .Tag }}' | grep -q koalaman/shellcheck-alpine:stable; then
+  docker pull koalaman/shellcheck-alpine:stable
+fi
+
 run_shellcheck()
 {
   docker run --rm --volume "$(pwd):/app" koalaman/shellcheck-alpine:stable /bin/sh -c 'find /app -type f ! -path "*/.git/*" ! -name "*.orig" -and \( -name "*.sh" -or -perm -0111 \) -print -exec shellcheck --exclude SC1008,SC1091 {} +'
