733875be9ccfb89dbd5d7f6b0eb92ef44666e85e
Kieren Evans <kevans@inviqa.com>
2021-03-17 10:49:32 +0200

    Speed up quality check

--- quality.sh
+++ quality.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-
 set -e -o pipefail
 
-docker pull koalaman/shellcheck:latest
+docker pull koalaman/shellcheck-alpine:stable
 run_shellcheck()
 {
-  local script="$1"
-  echo -n "Linting '$script': "
-  docker run --rm -i koalaman/shellcheck:latest --exclude SC1008,SC1091 - < "$script" && echo "OK"
+  docker run --rm --volume "$(pwd):/app" koalaman/shellcheck-alpine:stable /bin/sh -c 'find /app -type f ! -path "*/.git/*" ! -name "*.orig" -and \( -name "*.sh" -or -perm -0111 \) -exec shellcheck --exclude SC1008,SC1091 {} +'
 }
-export -f run_shellcheck
-
-find . -type f ! -path "./.git/*" ! -name "*.orig" -and \( -name "*.sh" -or -perm -0111 \) -exec bash -ec 'run_shellcheck "$0"' {} \;
+run_shellcheck
