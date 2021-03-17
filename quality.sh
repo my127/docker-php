7171116457a562398179ef20de9a22227f1eb48e
Kieren Evans <kevans@inviqa.com>
2021-03-17 10:54:03 +0200

    List files being looked at

--- quality.sh
+++ quality.sh
@@ -4,6 +4,6 @@ set -e -o pipefail
 docker pull koalaman/shellcheck-alpine:stable
 run_shellcheck()
 {
-  docker run --rm --volume "$(pwd):/app" koalaman/shellcheck-alpine:stable /bin/sh -c 'find /app -type f ! -path "*/.git/*" ! -name "*.orig" -and \( -name "*.sh" -or -perm -0111 \) -exec shellcheck --exclude SC1008,SC1091 {} +'
+  docker run --rm --volume "$(pwd):/app" koalaman/shellcheck-alpine:stable /bin/sh -c 'find /app -type f ! -path "*/.git/*" ! -name "*.orig" -and \( -name "*.sh" -or -perm -0111 \) -print -exec shellcheck --exclude SC1008,SC1091 {} +'
 }
 run_shellcheck
