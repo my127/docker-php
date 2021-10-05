f697b26138a4c48511fae2f8d4762f18ec440268
Kieren Evans <kevans@inviqa.com>
2021-10-05 18:52:12 +0300

    Improve newrelic download error handling

--- installer/stretch/extensions/newrelic.sh
+++ installer/stretch/extensions/newrelic.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 function install_newrelic()
-{
-    curl -L https://download.newrelic.com/php_agent/archive/9.16.0.295/newrelic-php5-9.16.0.295-linux.tar.gz | tar -C /tmp -zx
+(
+    set -o errexit -o pipefail
+
+    curl --fail --silent --show-error -L https://download.newrelic.com/php_agent/archive/9.16.0.295/newrelic-php5-9.16.0.295-linux.tar.gz -o '/tmp/newrelic.tar.gz'
+
+    tar -C /tmp -xzvf /tmp/newrelic.tar.gz
 
     export NR_INSTALL_USE_CP_NOT_LN=1
     export NR_INSTALL_SILENT=1
 
     # shellcheck disable=SC2211
     /tmp/newrelic-php5-*/newrelic-install install
-    rm -rf /tmp/newrelic-php5-* /tmp/nrinstall*
-}
+    rm -rf /tmp/newrelic-php5-* /tmp/nrinstall* /tmp/newrelic.tar.gz
+)
 
 function compile_newrelic()
 {
