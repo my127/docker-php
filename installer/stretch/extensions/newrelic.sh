07f3b5b037b184860f289e0ec358a47edba3f9a8
andytson-inviqa <andytson-inviqa@users.noreply.github.com>
2021-12-09 08:42:27 +0000

    Skip newrelic on arm64 due to not supporting it yet (#66)

--- installer/stretch/extensions/newrelic.sh
+++ installer/stretch/extensions/newrelic.sh
@@ -4,6 +4,11 @@ function install_newrelic()
 (
     set -o errexit -o pipefail
 
+    if [ "$(uname -m )" != x86_64 ]; then
+        echo "skipping newrelic extension, it is currently unsupported on architectures other than x86_64 / amd64" >&2
+        return 0
+    fi
+
     curl --fail --silent --show-error -L https://download.newrelic.com/php_agent/archive/9.16.0.295/newrelic-php5-9.16.0.295-linux.tar.gz -o '/tmp/newrelic.tar.gz'
 
     tar -C /tmp -xzvf /tmp/newrelic.tar.gz
