76eda32f1c0e05432a5da73b3a3507d0759b5711
Andy Thompson <athompson@inviqa.com>
2023-06-16 16:43:14 +0100

    Add libssl-dev when compiling libmemcached on bookworm
    libmemcached-dev is missing the libssl-dev dependency in it's deb pkg https://groups.google.com/g/linux.debian.bugs.dist/c/XZ9ymLnPXrA

---
+++ installer/bookworm/extensions/memcached.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function install_memcached()
+{
+    _memcached_deps_runtime
+
+    if ! has_extension memcached; then
+        compile_memcached
+    fi
+
+    docker-php-ext-enable memcached
+}
+
+function compile_memcached()
+{
+    _memcached_deps_build
+
+    case "$VERSION" in
+            "5.6")
+                printf "\n" | pecl install memcached-2.2.0
+                ;;
+            *)
+                printf "\n" | pecl install memcached
+    esac
+    _memcached_clean
+}
+
+function _memcached_deps_runtime()
+{
+    install libmemcached11 libmemcachedutil2
+}
+
+function _memcached_deps_build()
+{
+    install libmemcached-dev libssl-dev zlib1g-dev
+}
+
+function _memcached_clean()
+{
+    remove libmemcached-dev libssl-dev zlib1g-dev
+}
